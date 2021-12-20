4675c83eb1cebe89c1718dcc09e794ecb0e5827b
Rémi Marçais <remi.marcais87@gmail.com>
2021-12-20 18:54:29 +0100

    Fix bugs & improve code

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,21 +9,15 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *tmp = list;
+	listint_t *tmp;
 
 	if (list == NULL || list->next==NULL)
 		return (0);
-	while (tmp != NULL)
+	for (tmp = list->next->next, list = list->next; tmp && list;
+	     tmp = tmp->next->next, list = list->next)
        	{
-		if (tmp->next->next != NULL)
-		{
-			tmp = tmp->next->next;
-			list = list->next;
-			if (tmp == list)
-				return (1);
-		}
-		else
-			break;
+       		if (tmp == list)
+       			return (1);
 	}
 	return (0);
 }
