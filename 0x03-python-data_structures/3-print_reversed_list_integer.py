d73d4229035a5961462e1137774bb645d5eb6e0c
Rémi Marçais <remi.marcais87@gmail.com>
2022-01-03 12:29:58 +0100

    Add ':d' to the program

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     newlist = my_list[::-1]
     for i in range(0, len(newlist)):
-        print("{}".format(newlist[i]))
+        print("{:d}".format(newlist[i]))
