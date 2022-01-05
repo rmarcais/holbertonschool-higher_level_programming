04047a5f235367f714cd260724f16196ca30f160
Rémi Marçais <remi.marcais87@gmail.com>
2022-01-05 19:23:44 +0100

    Fix bugs

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return [list(map(lambda x: x * x, row)) for row in matrix]
+    return list(map(lambda r: list(map(lambda x: x * x, r)), matrix))
