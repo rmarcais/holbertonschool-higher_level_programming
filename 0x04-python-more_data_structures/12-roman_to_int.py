ae8d90d759abe91fbbb7672d77e8275c4245b771
Rémi Marçais <remi.marcais87@gmail.com>
2022-01-05 17:10:56 +0100

    Fix bugs

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,32 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    r = roman_string
+    rd = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     sum = 0
-    if roman_string is None:
-        return None
-    for char in range(len(roman_string)):
-        if char != len(roman_string) - 1:
-            if roman_string[char] == 'I' and roman_string[char + 1] == 'V':
-                sum += 4
-                char += 1
-                break
-            elif roman_string[char] == 'I' and roman_string[char + 1] == 'X':
-                sum += 9
-                char += 1
-                break
-        if roman_string[char] == 'I':
-            sum += 1
-        elif roman_string[char] == 'V':
-            sum += 5
-        elif roman_string[char] == 'X':
-            sum += 10
-        elif roman_string[char] == 'L':
-            sum += 50
-        elif roman_string[char] == 'C':
-            sum += 100
-        elif roman_string[char] == 'D':
-            sum += 500
-        elif roman_string[char] == 'M':
-            sum += 1000
+    if r is None or r == "":
+        return 0
+    for i in range(len(r)):
+        if r[i] not in rd:
+            return 0
+        if i > 0 and rd[r[i]] > rd[r[i - 1]]:
+            sum += rd[r[i]] - 2 * rd[r[i - 1]]
         else:
-            return None
+            sum += rd[r[i]]
     return sum
