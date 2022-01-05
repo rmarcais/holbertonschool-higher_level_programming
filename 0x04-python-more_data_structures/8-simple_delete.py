3f411ae66921df34c521200621f52bc45f10bb74
Rémi Marçais <remi.marcais87@gmail.com>
2022-01-05 14:22:17 +0100

    Add the 8-simple_delete.py file

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    for keys in a_dictionary:
+        if keys == key:
+            del a_dictionary[key]
+            break
+    return a_dictionary
