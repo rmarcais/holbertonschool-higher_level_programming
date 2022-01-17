8f5587779a674ec2c6e199c4585664f8a2405e44
Rémi Marçais <remi.marcais87@gmail.com>
2022-01-17 12:51:17 +0100

    Fix bugs by adding except(ValueError, TypeError)

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        a = 0
-        for i in range(0, x):
-            if isinstance(my_list[i], int) is True:
-                print("{:d}".format(my_list[i]), end="")
-            else:
-                a += 1
-        print("")
-    except:
-        raise
-    return i + 1 - a
+    count = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (TypeError, ValueError):
+            continue
+    print("")
+    return count
