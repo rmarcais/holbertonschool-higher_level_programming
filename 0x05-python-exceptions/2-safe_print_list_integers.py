6bc0ae3454d907bc371e560aabcdfeb38bd3c50e
Rémi Marçais <remi.marcais87@gmail.com>
2022-01-17 12:01:17 +0100

    Add all files

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        a = 0
+        for i in range(0, x):
+            if isinstance(my_list[i], int) is True:
+                print("{:d}".format(my_list[i]), end="")
+            else:
+                a += 1
+        print("")
+        return i + 1 - a
+    except IndexError:
+        raise
