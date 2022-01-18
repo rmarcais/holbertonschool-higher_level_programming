1ed5b60998102e560d9b5b8c4b3be7ed87d98959
Rémi Marçais <remi.marcais87@gmail.com>
2022-01-18 20:23:26 +0100

    Fix pycodestyle warnings

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """0-square.py
 
-Write an empty class Square that defines a square"""
+Write an empty class Square that defines a square
+"""
+
 
 class Square:
     """class that defines a square"""
