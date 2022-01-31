ef16fc779632ac457f23a696b85b9604b97d69d5
Rémi Marçais <remi.marcais87@gmail.com>
2022-01-31 15:36:03 +0100

    Add 100-my_int.py

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""100-my_int.py
+The class MyInt is a rebel. MyInt has == and != operators inverted.
+You are not allowed to import any module.
+"""
+
+
+class MyInt(int):
+    """class that inherits from int."""
+
+    """__init__ method that is called when a new instnace is created."""
+    def __init__(self, value):
+        self.value = value
+
+    def __eq__(self, other):
+        """Tests if an integer is equal to another integer.
+        It returns False if yes, True otherwise.
+        """
+        if self.value == other:
+            return False
+        return True
+
+    def __ne__(self, other):
+        """Tests if an integer is not equal to another integer.
+        It returns False if yes, True otherwise.
+        """
+        if self.value != other:
+            return False
+        return True
