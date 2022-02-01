59169369eabbdcde0d007f9f793ae414cea6eb61
Rémi Marçais <remi.marcais87@gmail.com>
2022-02-01 14:10:07 +0100

    Add 11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""11-student.py
+Module that defines a class Student.
+You are not allowed to import any module.
+"""
+
+
+class Student:
+    """Class that defines a student."""
+
+    """The __init__ method is called when a new instnace is created."""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Method that retrieves a dictionary representation
+        of a Student instance.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            my_dict = {}
+            for element in attrs:
+                if element in self.__dict__:
+                    my_dict[element] = self.__dict__[element]
+            return my_dict
+
+    def reload_from_json(self, json):
+        """Method that replaces all attributes of the Student instnance."""
+        for key in json:
+            self.__dict__[key] = json[key]
