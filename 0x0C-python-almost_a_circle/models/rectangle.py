aee1bd93c9c79cf651e2ac4bdbc9ac8cdb70ed74
Rémi Marçais <remi.marcais87@gmail.com>
2022-02-03 13:14:39 +0100

    Update display method: take care of x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -105,7 +105,11 @@ class Rectangle(Base):
         """Method that prints in stdout the Rectangle instance with
         the character '#'.
         """
+        for a in range(self.__y):
+            print()
         for i in range(self.__height):
+            for b in range(self.__x):
+                print(" ", end="")
             for j in range(self.__width):
                 print("#", end="")
             print()
