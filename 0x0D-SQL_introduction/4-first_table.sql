ab8bb3bc95a5f86ba51e77c58829045b5aad68eb
Rémi Marçais <remi.marcais87@gmail.com>
2022-03-08 13:29:25 +0100

    Edit files task 4, 9 and 12

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- This script creates a table called first_table in the current database in my SQL server.
 -- Creates the table.
 CREATE TABLE IF NOT EXISTS first_table (
-       id INTEGER DEFAULT NULL,
-       name VARCHAR(256) DEFAULT NULL
+       id INTEGER,
+       name VARCHAR(256)
 )
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,9 +1,9 @@
 -- This script creates a table second_table in the database hbtn_0c_0.
 -- Creates the table.
 CREATE TABLE IF NOT EXISTS second_table (
-       id INT DEFAULT NULL,
-       name VARCHAR(256) DEFAULT NULL,
-       score INT DEFAULT NULL
+       id INT,
+       name VARCHAR(256),
+       score INT
 );
 -- Inserts a new record.
 INSERT INTO second_table(id, name, score) VALUES (1, 'John', 10);
