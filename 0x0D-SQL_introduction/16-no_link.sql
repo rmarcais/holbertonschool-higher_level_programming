a21b22e331efdcbf2df60d2280efed506ffe57dd
Rémi Marçais <remi.marcais87@gmail.com>
2022-03-08 13:25:30 +0100

    Add 16-no_link.sql

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,9 @@
+-- This script lists all records of the table second_table.
+-- Don’t list rows without a name value
+-- Results should display the score and the name (in this order)
+-- Records should be listed by descending score
+-- Lists all records.
+SELECT score, name
+FROM second_table
+WHERE EXISTS (SELECT name FROM second_table)
+ORDER BY score DESC;
