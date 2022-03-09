b562e39ac29b4d5c5d3161fe24b12908ff35c9a8
Rémi Marçais <remi.marcais87@gmail.com>
2022-03-09 13:21:44 +0100

    Add 7-cities.sql

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,13 @@
+-- This script creates the database hbtn_0d_usa and the table cities.
+-- Create the database.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Using hbtn_0d_usa.
+USE hbtn_0d_usa;
+-- Creates the table.
+CREATE TABLE IF NOT EXISTS cities (
+       id INT AUTO_INCREMENT NOT NULL,
+       state_id INT NOT NULL,
+       name VARCHAR(256) NOT NULL,
+       PRIMARY KEY (id),
+       FOREIGN KEY (state_id) REFERENCES states(id)
+);
