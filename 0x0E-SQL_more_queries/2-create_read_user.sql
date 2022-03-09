c92f13ae1dc071dd98c6f9f603aaaf362b50fbf1
Rémi Marçais <remi.marcais87@gmail.com>
2022-03-09 12:25:23 +0100

    Add 2-create_read_user.sql

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- This script creates the database hbtn_0d_2 and the user user_0d_2.
+-- Creates the database.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Creates the user.
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- Set all privileges.
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_1'@'localhost';
