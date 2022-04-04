ef9233bb936d39668566b76732e8b5dd2cb9c07d
Rémi Marçais <remi.marcais87@gmail.com>
2022-04-04 14:28:41 +0200

    Add 8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const string = [];
+let i;
+const args = process.argv;
+let nb;
+if (isNaN(args[2]) === true) {
+  console.log('Missing size');
+} else {
+  nb = parseInt(args[2]);
+  for (i = 0; i < nb; i++) {
+    string.push('X');
+  }
+  for (i = 0; i < nb; i++) {
+    console.log(string.join(''));
+  }
+}
