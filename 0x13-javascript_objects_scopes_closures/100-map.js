e43921a9e66bb4b4969a160f941a0f0e91bb054a
Rémi Marçais <remi.marcais87@gmail.com>
2022-04-05 14:47:26 +0200

    Fix bugs

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-const newlist = list.map(x => x * list.indexOf(x));
 console.log(list);
-console.log(newlist);
+console.log(list.map((x, index) => x * index));
