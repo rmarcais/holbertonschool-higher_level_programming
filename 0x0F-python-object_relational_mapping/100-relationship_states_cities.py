2699210af76af8d81a2a73a617d85ce9fc8dcb45
Rémi Marçais <remi.marcais87@gmail.com>
2022-04-10 20:56:33 +0200

    Fix bugs

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -12,16 +12,11 @@ import sys
 
 
 if __name__ == "__main__":
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True,
-    )
-    Base.metadata.create_all(engine)
-    session = Session(engine)
-    newState = State(name="California")
-    newState.cities.append(City(name="San Francisco"))
-    session.add(newState)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    new_state = State(name="California")
+    new_state.cities.append(City(name="San Francisco"))
+    session.add(new_state)
     session.commit()
     session.close()
