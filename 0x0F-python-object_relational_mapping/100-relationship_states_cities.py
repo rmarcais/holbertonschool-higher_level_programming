dcf1242d8914ea808d9c6ab6c37de14d183e6d6f
Rémi Marçais <remi.marcais87@gmail.com>
2022-04-10 21:33:11 +0200

    fix bugs

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     Session.configure(bind=engine)
     session = Session()
     new_state = State(name="California")
-    new_state.cities.append(City(name="San Fransisco"))
+    new_state.cities.append(City(name="San Francisco"))
     session.add(new_state)
     session.commit()
     session.close()
