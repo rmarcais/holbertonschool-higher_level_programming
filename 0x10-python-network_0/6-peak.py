cd153fdc716039e07e849ff03d95f6d306f2f905
Rémi Marçais <remi.marcais87@gmail.com>
2022-04-21 15:20:47 +0200

    Improve code

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,19 +2,27 @@
 """ Module containing the function find_peak """
 
 
+def find_peak_2(li, low, high, n):
+    """ Function that returns a peak of a list of integers. """
+    middle = int(low + (high - low) / 2)
+
+    if (middle == 0 or li[middle] >= li[middle - 1]) and \
+       (middle == n - 1 or li[middle] >= li[middle + 1]):
+        return li[middle]
+
+    elif middle > 0 and li[middle] < li[middle - 1]:
+        return find_peak_2(li, low, middle - 1, n)
+
+    else:
+        return find_peak_2(li, middle + 1, high, n)
+
+
 def find_peak(list_of_integers):
     """ Function that find a peak in a list of integers. """
 
-    if list_of_integers == [] or list_of_integers is None:
+    if list_of_integers is None or list_of_integers == []:
         return None
 
-    li = list_of_integers
-    ln = len(li)
+    ln = len(list_of_integers)
 
-    if ln == 1:
-        return li[0]
-
-    if li[int(ln / 2)] <= li[int(ln / 2 - 1)]:
-        return(find_peak(li[0:int(ln / 2)]))
-    else:
-        return(find_peak(li[int(ln / 2):int(ln)]))
+    return find_peak_2(list_of_integers, 0, ln - 1, ln)
