b37b88d066519d390f1c4cc660535e9a03b3827a
Rémi Marçais <remi.marcais87@gmail.com>
2022-04-21 14:29:55 +0200

    Fix bugs

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,13 +8,14 @@ def find_peak(list_of_integers):
     if list_of_integers == [] or list_of_integers is None:
         return None
 
-    li = list_of_integers
-    ln = len(li)
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
 
-    if ln == 1:
-        return li[0]
-
-    if li[int(ln / 2)] <= li[int(ln / 2 - 1)]:
-        return(find_peak(li[0:int(ln / 2)]))
+    if list_of_integers[int(len(list_of_integers)
+                            / 2)] <= list_of_integers[int(len(list_of_integers)
+                                                          / 2 - 1)]:
+        return(find_peak(list_of_integers[0:int(len(list_of_integers) / 2)]))
     else:
-        return(find_peak(li[int(ln / 2):int(ln)]))
+        return(find_peak(list_of_integers[
+            int(len(list_of_integers)
+                / 2):int(len(list_of_integers))]))
