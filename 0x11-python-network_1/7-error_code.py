4cb2c3093bf55c790d2db69ad36c6be282f2cd00
Rémi Marçais <remi.marcais87@gmail.com>
2022-04-22 12:12:27 +0200

    Fix bugs

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,8 +8,10 @@ the body of the response.
 import requests
 from sys import argv
 
-r = requests.get(argv[1])
-if r.status_code < 400:
-    print(r.text)
-else:
-    print("Error code: {}".format(r.status_code))
+
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+    if r.status_code < 400:
+        print(r.text)
+    else:
+        print("Error code: {}".format(r.status_code))
