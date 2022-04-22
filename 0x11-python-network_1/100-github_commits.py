ce2650773574e622766e433e132a14f94a2f2785
Rémi Marçais <remi.marcais87@gmail.com>
2022-04-22 14:47:16 +0200

    Put old version

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -16,10 +16,7 @@ if __name__ == "__main__":
     url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
     r = requests.get(url)
     count = 0
-    for committer in r.json():
-        if count == 10:
-            break
-        sha = committer.get("sha")
-        aname = committer.get("commit").get("author").get("name")
+    for i in range(10):
+        sha = r.json()[i].get("sha")
+        aname = r.json()[i].get("commit").get("author").get("name")
         print("{}: {}".format(sha, aname))
-        count += 1
