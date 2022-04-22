6da194937f2e9320694d6afbbce5f343b93908f9
Rémi Marçais <remi.marcais87@gmail.com>
2022-04-22 12:03:35 +0200

    Fix bugs

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,10 @@
 
 import urllib.request as u
 
-with u.urlopen('https://intranet.hbtn.io/status') as r:
-    html = r.read()
-print("Body response:")
-print("\t- type: {}".format(type(html)))
-print("\t- content: {}".format(html))
-print("\t- utf8 content: {}".format(html.decode('utf8')))
+if __name__ == "__main__":
+    with u.urlopen('https://intranet.hbtn.io/status') as r:
+        html = r.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf8')))
