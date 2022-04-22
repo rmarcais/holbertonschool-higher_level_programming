01d3ab72d96543f597c1f1dd490bc21e37ccd72f
Rémi Marçais <remi.marcais87@gmail.com>
2022-04-22 10:28:12 +0200

    Add 2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email, sends a POST request to the
+passed URL with the email as a parameter, and displays the body of the
+response (decoded in utf-8)
+"""
+
+
+from urllib import parse, request
+from sys import argv
+
+url = argv[1]
+value = {'email': argv[2]}
+data = parse.urlencode(value)
+data = data.encode('ascii')
+req = request.Request(url, data)
+with request.urlopen(req) as r:
+    the_page = r.read()
+print(the_page.decode('utf8'))
