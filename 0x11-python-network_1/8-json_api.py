c5de498df53fa738fe088cbfab70b74de516539e
Rémi Marçais <remi.marcais87@gmail.com>
2022-04-22 12:14:01 +0200

    Fix bugs

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,16 +6,18 @@ http://0.0.0.0:5000/search_user with the letter as a parameter.
 
 import requests
 from sys import argv
-if len(argv) < 2:
-    d = {'q': ""}
-else:
-    d = {'q': argv[1]}
-r = requests.post('http://0.0.0.0:5000/search_user',
-                  data=d)
-try:
-    if r.json() == {}:
-        print("No result")
+
+if __name__ =="__main__":
+    if len(argv) < 2:
+        d = {'q': ""}
     else:
-        print("[{}] {}".format(r.json()["id"], r.json()["name"]))
-except Exception:
-    print("Not a valid JSON")
+        d = {'q': argv[1]}
+    r = requests.post('http://0.0.0.0:5000/search_user',
+                      data=d)
+    try:
+        if r.json() == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(r.json()["id"], r.json()["name"]))
+    except Exception:
+        print("Not a valid JSON")
