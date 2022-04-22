cb737b75695d192e1ce3db2dcafe6b4c1bb53051
Rémi Marçais <remi.marcais87@gmail.com>
2022-04-22 12:08:28 +0200

    Fix bugs

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,8 +6,10 @@ Python script that fetches https://intranet.hbtn.io/status
 
 import requests
 
-r = requests.get('https://intranet.hbtn.io/status')
 
-print("Body response:")
-print("\t- type: {}".format(type(r.text)))
-print("\t- content: {}".format(r.text))
+if __name__ == "__main__":
+    r = requests.get('https://intranet.hbtn.io/status')
+
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
