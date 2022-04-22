04a83d620ba710711aad3a3a98693a28dca662d5
Rémi Marçais <remi.marcais87@gmail.com>
2022-04-22 14:35:12 +0200

    Fix bugs

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -15,7 +15,11 @@ from sys import argv
 if __name__ == "__main__":
     url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
     r = requests.get(url)
-    for i in range(10):
-        sha = r.json()[i].get("sha")
-        aname = r.json()[i].get("commit").get("author").get("name")
+    count = 0
+    for committer in r.json():
+        if count == 10:
+            break
+        sha = committer.get("sha")
+        aname = committer.get("commit").get("author").get("name")
         print("{}: {}".format(sha, aname))
+        count += 1
