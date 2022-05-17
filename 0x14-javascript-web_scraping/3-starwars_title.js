b624f4e417a305f4c1851b4ef1fd905439f59b28
Rémi Marçais <remi.marcais87@gmail.com>
2022-05-17 12:06:26 +0200

    Fix bugs

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const args = process.argv;
-const axios = require('axios').default;
+const axios = require('axios')
 
 axios.get('https://swapi-api.hbtn.io/api/films/' + args[2])
   .then(function (response) {
