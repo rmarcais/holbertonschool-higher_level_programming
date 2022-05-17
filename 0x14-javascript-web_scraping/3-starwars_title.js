fbd13cc61f426d9f0873b88cc6e6f29bf780d20b
Rémi Marçais <remi.marcais87@gmail.com>
2022-05-17 11:46:02 +0200

    Add 3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const args = process.argv;
+const axios = require('axios').default;
+
+axios.get('https://swapi-api.hbtn.io/api/films/' + args[2])
+  .then(function (response) {
+    console.log(response.data.title);
+  });
