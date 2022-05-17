6382813a12737a19edd0d87ec82838c0c6f4f4f7
Rémi Marçais <remi.marcais87@gmail.com>
2022-05-17 13:12:12 +0200

    Add 6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const args = process.argv;
+const axios = require('axios').default;
+let i;
+let tmp;
+let count = 0;
+const dico = {};
+
+axios.get(args[2])
+  .then(function (response) {
+    for (i = 0; i < response.data.length; i++) {
+      if (tmp !== response.data[i].userId) {
+        count = 0;
+      }
+      if (response.data[i].completed === true) {
+        count += 1;
+        dico[response.data[i].userId] = count;
+      }
+      tmp = response.data[i].userId;
+    }
+    console.log(dico);
+  });
