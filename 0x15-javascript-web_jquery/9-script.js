b64ea9506d98d1cee3b4c5c276d64a1c7b4914da
Rémi Marçais <remi.marcais87@gmail.com>
2022-05-25 13:56:35 +0200

    Check if the document is ready before execute the script.

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,3 +1,5 @@
-$.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data, status) {
-  $('#hello').text(data.hello);
+$(document).ready(function () {
+  $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data, status) {
+    $('#hello').text(data.hello);
+  });
 });
